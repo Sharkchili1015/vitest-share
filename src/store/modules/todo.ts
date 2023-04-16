aa2a33bcf511c81ee16018d1ec2c3db6f2818e96
Ricky <1372896443@qq.com>
2023-04-16 23:16:54 +0800

    feat:init

---
+++ src/store/modules/todo.ts
@@ -0,0 +1,60 @@
+import { defineStore } from "pinia";
+import { ref, watch } from "vue";
+type Todo = {
+  done: boolean;
+  content: string;
+};
+export const useTodotore = defineStore("todo", () => {
+  const count = ref(0);
+  const add = () => {
+    count.value++;
+  };
+  const defaultData = [
+    {
+      done: false,
+      content: "Write a blog post",
+    },
+  ];
+  const todoList = ref<Todo[]>([]);
+  watch(
+    () => todoList.value,
+    () => {
+      if (todoList.value.length === 0) {
+        todoList.value.push(...defaultData);
+      }
+    },{
+      immediate:true
+    }
+  );
+  const addTodo = (todo: string) => {
+    const isTop = todo.includes("top:");
+    const isDone = todo.includes("done:");
+    const addContent = todo.replace("top:", "").replace("done:", "");
+    if (isTop) {
+      todoList.value.unshift({
+        done: isDone,
+        content: addContent,
+      });
+      return;
+    }
+    todoList.value.push({
+      done: isDone,
+      content: addContent,
+    });
+  };
+  const removeTodo = (index: number) => {
+    todoList.value.splice(index, 1);
+    return todoList.value[index]
+  };
+  const doneTodo = (index:number) =>{
+    todoList.value[index].done = !todoList.value[index].done
+  }
+  return {
+    count,
+    add,
+    addTodo,
+    removeTodo,
+    todoList,
+    doneTodo
+  };
+});
---
+++ src/tests/index.test.ts
@@ -0,0 +1,60 @@
+import { describe, expect, it, beforeEach } from "vitest";
+import { createPinia, setActivePinia } from "pinia";
+import { useTodotore } from "../store/modules/todo";
+
+describe("todoStore", () => {
+  beforeEach(() => {
+    setActivePinia(createPinia());
+  });
+
+  it("调用add函数count应该+1", () => {
+    const store = useTodotore();
+    store.add();
+    expect(store.count).toBe(1);
+  });
+
+  it("使用top:命令应该将todo放到第一位", () => {
+    const store = useTodotore();
+    store.addTodo("top:1");
+    expect(store.todoList[0].content).toBe("1");
+  });
+
+  it("使用top:和done:命令应该将todo放到第一位状态为done", () => {
+    const store = useTodotore();
+    store.addTodo("top:done:1");
+    expect(store.todoList[0]).toEqual({
+        done: true,
+        content: "1",
+    });
+  });
+
+  it("使用done:命令应该添加一个done为true的todo在数组末尾", () => {
+    const store = useTodotore();
+    store.addTodo("done:1");
+    expect(store.todoList[store.todoList.length -1]).toEqual({
+        done: true,
+        content: "1",
+    });
+  });
+
+  it("调用doneTodo应该将对应index的done状态改变为true", () => {
+    const store = useTodotore();
+    store.addTodo("应该为done为true")
+    store.doneTodo(1);
+    expect(store.todoList[1]).toEqual({
+        done:true,
+        content:"应该为done为true"
+    })
+  });
+
+  it("调用removeDone应该将对应index的todo删除", () => {
+    const store = useTodotore();
+    store.addTodo("1")
+    store.addTodo("2")
+    store.addTodo("3")
+    const remove = store.removeTodo(2)
+    const remover = store.removeTodo(1)
+    expect(store.todoList).toContain(remove)
+    expect(store.todoList).toContain(remover)
+  });
+});
